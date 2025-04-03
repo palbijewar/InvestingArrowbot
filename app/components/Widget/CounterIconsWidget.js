1830448d178e9231cc404ec8d0e9dd7a24c34631
palbijewar <pal.bijewar@gmail.com>
2025-04-03 14:59:35 +0530

    added: counter cards in thbe dashboard

--- app/components/Widget/CounterIconsWidget.js
+++ app/components/Widget/CounterIconsWidget.js
@@ -4,61 +4,92 @@ import Grid from '@mui/material/Grid';
 import OndemandVideo from '@mui/icons-material/OndemandVideo';
 import SupervisorAccount from '@mui/icons-material/SupervisorAccount';
 import CollectionsBookmark from '@mui/icons-material/CollectionsBookmark';
-import Edit from '@mui/icons-material/Edit';
+import AccountBalanceWallet from '@mui/icons-material/AccountBalanceWallet';
+import TrendingUp from '@mui/icons-material/TrendingUp';
+import Assessment from '@mui/icons-material/Assessment';
+import Group from '@mui/icons-material/Group';
+import BusinessCenter from '@mui/icons-material/BusinessCenter';
+import MonetizationOn from '@mui/icons-material/MonetizationOn';
+import ShowChart from '@mui/icons-material/ShowChart';
+import EmojiEvents from '@mui/icons-material/EmojiEvents';
 import { injectIntl } from 'react-intl';
 import CounterWidget from '../Counter/CounterWidget';
-import messages from './messages';
 import useStyles from './widget-jss';
 
-function CounterIconWidget(props) {
-  const { intl } = props;
+function CounterIconWidget() {
   const { classes } = useStyles();
 
   return (
     <div className={classes.rootCounterFull}>
       <Grid container spacing={2}>
         <Grid item xs={6} md={3}>
-          <CounterWidget
-            color="secondary-dark"
-            start={0}
-            end={207}
-            duration={3}
-            title={intl.formatMessage(messages.subscribers)}
-          >
+          <CounterWidget color="secondary-dark" start={0} end={0} duration={3} title="Bot Activation">
             <OndemandVideo className={classes.counterIcon} />
           </CounterWidget>
         </Grid>
         <Grid item xs={6} md={3}>
-          <CounterWidget
-            color="secondary-main"
-            start={0}
-            end={300}
-            duration={3}
-            title={intl.formatMessage(messages.followers)}
-          >
+          <CounterWidget color="secondary-main" start={0} end={0} duration={3} title="Day's Remaining">
+            <Assessment className={classes.counterIcon} />
+          </CounterWidget>
+        </Grid>
+        <Grid item xs={6} md={3}>
+          <CounterWidget color="secondary-main" start={0} end={0} duration={3} title="Portfolio Investment">
+            <BusinessCenter className={classes.counterIcon} />
+          </CounterWidget>
+        </Grid>
+        <Grid item xs={6} md={3}>
+          <CounterWidget color="secondary-main" start={0} end={0} duration={3} title="Gas Wallet">
+            <AccountBalanceWallet className={classes.counterIcon} />
+          </CounterWidget>
+        </Grid>
+        <Grid item xs={6} md={3}>
+          <CounterWidget color="secondary-dark" start={0} end={0} duration={3} title="Portfolio Income">
+            <MonetizationOn className={classes.counterIcon} />
+          </CounterWidget>
+        </Grid>
+        <Grid item xs={6} md={3}>
+          <CounterWidget color="secondary-main" start={0} end={0} duration={3} title="Rank Income">
+            <EmojiEvents className={classes.counterIcon} />
+          </CounterWidget>
+        </Grid>
+        <Grid item xs={6} md={3}>
+          <CounterWidget color="secondary-main" start={0} end={0} duration={3} title="Level Income">
+            <TrendingUp className={classes.counterIcon} />
+          </CounterWidget>
+        </Grid>
+        <Grid item xs={6} md={3}>
+          <CounterWidget color="secondary-main" start={0} end={0} duration={3} title="Trading Portfolio">
+            <ShowChart className={classes.counterIcon} />
+          </CounterWidget>
+        </Grid>
+        <Grid item xs={6} md={3}>
+          <CounterWidget color="secondary-dark" start={0} end={0} duration={3} title="Direct Team">
+            <Group className={classes.counterIcon} />
+          </CounterWidget>
+        </Grid>
+        <Grid item xs={6} md={3}>
+          <CounterWidget color="secondary-main" start={0} end={0} duration={3} title="Downline Team">
             <SupervisorAccount className={classes.counterIcon} />
           </CounterWidget>
         </Grid>
         <Grid item xs={6} md={3}>
-          <CounterWidget
-            color="secondary-main"
-            start={0}
-            end={67}
-            duration={3}
-            title={intl.formatMessage(messages.total_posts)}
-          >
-            <Edit className={classes.counterIcon} />
+          <CounterWidget color="secondary-main" start={0} end={0} duration={3} title="Downline Bot Business">
+            <CollectionsBookmark className={classes.counterIcon} />
           </CounterWidget>
         </Grid>
         <Grid item xs={6} md={3}>
-          <CounterWidget
-            color="secondary-main"
-            start={0}
-            end={70}
-            duration={3}
-            title={intl.formatMessage(messages.total_articles)}
-          >
-            <CollectionsBookmark className={classes.counterIcon} />
+          <CounterWidget color="secondary-main" start={0} end={0} duration={3} title="Downline Portfolio Business">
+            <BusinessCenter className={classes.counterIcon} />
+          </CounterWidget>
+        </Grid>
+        <Grid item xs={6} md={3}>
+          <CounterWidget color="secondary-main" start={0} end={0} duration={3} title="Total Balance">
+            <AccountBalanceWallet className={classes.counterIcon} />
+          </CounterWidget>
+        </Grid>
+        <Grid item xs={6} md={3}>
+          <CounterWidget color="secondary-main" start={0} end={0} duration={3} title="Current Rank">
+            <EmojiEvents className={classes.counterIcon} />
           </CounterWidget>
         </Grid>
       </Grid>
--- app/containers/Dashboard/AnalyticDashboard.js
+++ app/containers/Dashboard/AnalyticDashboard.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import brand from 'enl-api/dummy/brand';
 import { Helmet } from 'react-helmet';
-import useMediaQuery from '@mui/material/useMediaQuery';
 import Grid from '@mui/material/Grid';
 import Divider from '@mui/material/Divider';
 import {
   CounterIconsWidget,
-  PerformanceChartWidget,
-  DateWidget,
-  TaskWidget,
-  WeatherWidget,
-  ContactWidget,
-  TimelineWidget,
-  FilesWidget,
 } from 'enl-components';
 import useStyles from './dashboard-jss';
 
@@ -21,7 +13,6 @@ function AnalyticDashboard() {
   const description = brand.desc;
 
   const { classes } = useStyles();
-  const lgDown = useMediaQuery(theme => theme.breakpoints.down('lg'));
 
   return (
     <div>
@@ -40,33 +31,6 @@ function AnalyticDashboard() {
         </Grid>
       </Grid>
       <Divider className={classes.divider} />
-      {/* 2nd Section */}
-      <Grid container spacing={3} className={classes.root}>
-        <Grid item xs={12}>
-          <PerformanceChartWidget />
-        </Grid>
-      </Grid>
-      {/* 3rd Section */}
-      <Grid container spacing={3} className={classes.root}>
-        <Grid item md={6} xs={12}>
-          <Divider className={classes.divider} />
-          <ContactWidget />
-          <Divider className={classes.divider} />
-          <TaskWidget />
-        </Grid>
-        <Grid item md={6} xs={12}>
-          {!lgDown && (
-            <Divider className={classes.divider} />
-          )}
-          <WeatherWidget />
-          <Divider className={classes.divider} />
-          <DateWidget />
-          <Divider className={classes.divider} />
-          <TimelineWidget />
-        </Grid>
-      </Grid>
-      <Divider className={classes.divider} />
-      <FilesWidget />
     </div>
   );
 }
